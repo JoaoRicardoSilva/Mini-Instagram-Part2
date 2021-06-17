85c634605d421cc0284ba23bdca7e9c7e17915a1
João Ricardo <joaoricardopratasilva@hotmail.com>
2021-06-17 12:57:16 +0100

    Sign up page HTML, started CSS
    main.js: Come from repository "Mini-Instagram-Part1"

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mini Instagram - Part 2
+
+**Objective:** Do a program with an Instagram like logic using DOM manipulation.
+
+**Note:** The starting point for main.js was the repository "Mini-Instagram-Part1"
---
+++ main.js
@@ -0,0 +1,303 @@
+"use strict";
+
+const miniInstagram = () => {
+    // Base class for new Users
+    class User {
+        constructor(name, email, password) {
+            this.name = name;
+            this.email = email;
+            this.password = password;
+            this.followers = 0;
+            this.following = 0;
+        }
+    }
+
+    // Memory to save new Users
+    const memory = [];
+
+    // Online state:
+    let online = false;
+
+    // Index in the memory for the user that have log
+    let indexUser = -1;
+
+    // Generic function for the user give some input
+    const askUser = (str) => window.prompt(str);
+
+    // COMMAND LOG IN
+    const logIn = () => {
+        let index = -1;
+        let email = null;
+        let pass = null;
+
+        //If is someone already online
+        if (online) {
+            alert("You are already logged in");
+            alert("");
+            return;
+        }
+
+        email = askUser("Enter your email!").trim();
+        pass = askUser("Enter your password!");
+
+        // Check if email exist in memory
+        const checkEmail = () => {
+            for (let i = 0; i < memory.length; i++) {
+                if (memory[i].email === email) {
+                    index = i;
+                }
+            }
+        };
+        checkEmail();
+
+        //If email doesn't exist
+        if (index < 0) {
+            alert("We don't have that account");
+            alert("");
+            return;
+        }
+
+        //If the password isn't correct
+        if (index > -1) {
+            if (memory[index].password !== pass) {
+                alert("The password is incorrect");
+                alert("");
+                return;
+            }
+        }
+
+        online = true;
+        //Save outside this function the index in memory of the user that logged
+        indexUser = index;
+        alert(`Welcome, ${memory[index].name}.`);
+        alert("");
+        return;
+    };
+
+    // COMMAND SIGN UP
+    const signUp = () => {
+        let name = null;
+        let email = null;
+        let stop = false;
+
+        //Check if someone is online
+        if (online) {
+            alert("log out first before you create a new account");
+            alert("");
+            return true;
+        }
+
+        name = askUser("Enter your name!");
+        email = askUser("Enter your email!").trim();
+
+        let emailLoop = () => {
+            //Command for exit askEmail
+            if (/^exit\*/gi.test(email)) {
+                stop = true;
+                return false;
+            }
+
+            //Check if is a valid email
+            if (
+                !/^([a-zA-Z0-9\_]+\@[a-zA-Z]+\.[a-zA-Z]+\.[a-zA-Z]+)|^([a-zA-Z0-9\_]+\@[a-zA-Z]+\.[a-zA-Z]+)/.test(
+                    email
+                )
+            ) {
+                alert("Insert a valid email");
+                alert("");
+                return true;
+            }
+
+            //Check if email already exist
+            const emailExist = () => {
+                let n = 0;
+                for (let i = 0; i < memory.length; i++) {
+                    if (memory[i].email === email) {
+                        n = 1;
+                    }
+                }
+                console.log({ memory });
+                if (n === 1) {
+                    alert("Sorry, that email is already taken");
+                    alert("");
+                    return true;
+                }
+            };
+            if (emailExist()) {
+                return true;
+            }
+        };
+
+        while (emailLoop()) {
+            email = askUser("Enter your email!");
+        }
+
+        if (stop === true) {
+            return;
+        }
+
+        // Create a new user based on User class
+        const newProfile = new User(
+            name,
+            email,
+            askUser("Enter your password!")
+        );
+
+        alert("Thank you for your registration, welcome!");
+        alert("");
+
+        memory.push(newProfile);
+        return;
+    };
+
+    // COMMAND EXIT
+    const exit = () => alert("You left the program, bye");
+
+    // COMMAND SEARCH
+    const search = () => {
+        let index = -1;
+        let email = null;
+
+        //Check if User is logged
+        const log0 = () => {
+            if (online === false) {
+                alert(
+                    "Sorry, you have to be logged in to use that functionality"
+                );
+                alert("");
+                return true;
+            }
+        };
+        if (log0()) {
+            return;
+        }
+
+        email = askUser("Enter your email").trim();
+
+        // Check if email exist in memory
+        const checkEmail = () => {
+            for (let i = 0; i < memory.length; i++) {
+                if (memory[i].email === email) {
+                    index = i;
+                }
+            }
+
+            //If it doesn't
+            if (index < 0) {
+                alert("We have no results for that query");
+                return true;
+            }
+        };
+        if (checkEmail()) {
+            return;
+        }
+
+        alert(
+            `${memory[index].name}\n${memory[index].email}\nFollowers: ${memory[index].followers}\nFollowing: ${memory[index].following}`
+        );
+        alert("");
+    };
+    // COMMAND LOG OUT
+    const logOut = () => {
+        if (online === false) {
+            alert("Sorry, you have to be logged in to use that functionality");
+            alert("");
+            return;
+        }
+
+        online = false;
+
+        alert("You logged out, see you later");
+        alert("");
+        return;
+    };
+
+    // Command Follow
+    const follow = () => {
+        if (online === false) {
+            alert("Sorry, you have to be logged in to use that functionality");
+            alert("");
+            return;
+        }
+
+        const followEmail = askUser(
+            "What's the email of the person that you want to folloW?"
+        );
+        let index = -1;
+
+        //Check if email already exist
+        const emailExist = () => {
+            for (let i = 0; i < memory.length; i++) {
+                if (memory[i].email === followEmail) {
+                    index = i;
+                }
+            }
+
+            if (index === -1) {
+                alert("That user does not exist");
+                alert("");
+                return true;
+            }
+        };
+        if (emailExist()) {
+            return;
+        }
+
+        // Add 1 follower to the user that was search
+        memory[index].followers++;
+
+        // Add 1 following to the User
+        memory[indexUser].following++;
+
+        alert(`You now follow ${memory[index].name}`);
+        alert("");
+    };
+
+    // CHECK ANS EXECUTE THE COMMANDS
+    const commandsSwitch = () => {
+        // Get the command from the user
+        let command = askUser("What's your command?");
+        console.log({ command });
+        const regex = command.match(/^([a-zA-Z]+ [a-zA-Z]+)|([a-zA-Z]+)/g);
+        console.log(regex);
+
+        switch (regex[0].toLowerCase()) {
+            case "log in":
+                logIn();
+                console.log({ memory });
+                commandsSwitch();
+                break;
+            case "sign up":
+                signUp();
+                console.log({ memory });
+                commandsSwitch();
+                break;
+            case "exit":
+                exit();
+                break;
+            case "search":
+                search();
+                console.log({ memory });
+                commandsSwitch();
+                break;
+            case "log out":
+                logOut();
+                console.log({ memory });
+                commandsSwitch();
+                break;
+            case "follow":
+                follow();
+                console.log({ memory });
+                commandsSwitch();
+                break;
+
+            default:
+                window.alert("We don't have that option");
+                alert("");
+                commandsSwitch();
+                break;
+        }
+    };
+    commandsSwitch();
+};
+miniInstagram();
