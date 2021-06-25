295918a52460655b4af7e93923beeb58c51e2ea4
João Ricardo <joaoricardopratasilva@hotmail.com>
2021-06-25 12:59:55 +0100

    started publish(); created html/css for img

--- main.js
+++ main.js
@@ -205,7 +205,7 @@ const logOut = () => {
     return;
 };
 
-// Command Follow
+// COMMAND FOLLOW
 const follow = () => {
     let index = -1;
     let followEmail = document
@@ -244,49 +244,7 @@ const follow = () => {
     return;
 };
 
-// CHECK AND EXECUTE THE COMMANDS
-const commandsSwitch = () => {
-    // Get the command from the user
-    let command = askUser("What's your command?");
-    console.log({ command });
-    const regex = command.match(/^([a-zA-Z]+ [a-zA-Z]+)|([a-zA-Z]+)/g);
-    console.log(regex);
-
-    switch (regex[0].toLowerCase()) {
-        case "log in":
-            logIn();
-            console.log({ memory });
-            commandsSwitch();
-            break;
-        case "sign up":
-            signUp();
-            console.log({ memory });
-            commandsSwitch();
-            break;
-        case "exit":
-            exit();
-            break;
-        case "search":
-            search();
-            console.log({ memory });
-            commandsSwitch();
-            break;
-        case "log out":
-            logOut();
-            console.log({ memory });
-            commandsSwitch();
-            break;
-        case "follow":
-            follow();
-            console.log({ memory });
-            commandsSwitch();
-            break;
-
-        default:
-            window.alert("We don't have that option");
-            alert("");
-            commandsSwitch();
-            break;
-    }
+const publish = () => {
+    const url = document.querySelector("#dom__photo-url").value;
+    const desc = document.querySelector("#dom__photo-desc").value;
 };
-// commandsSwitch();
---
+++ other.js
@@ -0,0 +1,46 @@
+// CHECK AND EXECUTE THE COMMANDS
+const commandsSwitch = () => {
+    // Get the command from the user
+    let command = askUser("What's your command?");
+    console.log({ command });
+    const regex = command.match(/^([a-zA-Z]+ [a-zA-Z]+)|([a-zA-Z]+)/g);
+    console.log(regex);
+
+    switch (regex[0].toLowerCase()) {
+        case "log in":
+            logIn();
+            console.log({ memory });
+            commandsSwitch();
+            break;
+        case "sign up":
+            signUp();
+            console.log({ memory });
+            commandsSwitch();
+            break;
+        case "exit":
+            exit();
+            break;
+        case "search":
+            search();
+            console.log({ memory });
+            commandsSwitch();
+            break;
+        case "log out":
+            logOut();
+            console.log({ memory });
+            commandsSwitch();
+            break;
+        case "follow":
+            follow();
+            console.log({ memory });
+            commandsSwitch();
+            break;
+
+        default:
+            window.alert("We don't have that option");
+            alert("");
+            commandsSwitch();
+            break;
+    }
+};
+// commandsSwitch();
